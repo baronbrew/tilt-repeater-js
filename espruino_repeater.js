b0f1ec608d6eeaca7243546937b31b0d565fc5f3
Noah Baron <noahbaron@gmail.com>
2019-02-07 13:13:58 -0800

    Optimizations

--- espruino_repeater.js
+++ espruino_repeater.js
@@ -1,56 +1,53 @@
-function arrayBufferToHex (arrayBuffer){
-var view = new Uint8Array(arrayBuffer);
-var result = '';
-var value;
-for (var i = 0; i < view.length; i++) {
-    value = view[i].toString(16);
-    result += (value.length === 1 ? '0' + value : value);
-  }
+// Use "save on send" -> "direct to flash" in Web IDE settings
+// Needs 2v00 or above - preferably 2v01 or later
 
-  return result;
+function arrayBufferToHex (arrayBuffer){
+  return (new Uint8Array(arrayBuffer)).slice().map(x=>(256+x).toString(16).substr(-2)).join("");
 }
 
 function startScan (){
-NRF.setScan(function(d) {
-if ( d.manufacturerData !== undefined ){
-var hexData = arrayBufferToHex(d.manufacturerData);
-  if (hexData[8] + hexData[9] + hexData[10] + hexData[11] == 'bb' + color + '0'){
-    changeInterval(tiltInterval, 980); 
-    digitalPulse(LED2, 1, 3);
-     NRF.setScan();
-     //console.log(hexData);
-     var majorValue = '0x' + hexData[36] + hexData[37] + hexData[38] + hexData[39];
-     var minorValue = '0x' + hexData[40] + hexData[41] + hexData[42] + hexData[43];
-     var uuidValue = parseInt('0x' + color + hexData[11]);
-     NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : majorValue, minor : minorValue, rssi : -59 }),{interval:1000});
-     scanCounter = 0;
+  NRF.setScan(function(d) {
+    var hexData = arrayBufferToHex(d.manufacturerData);
+    if (hexData.substr(8,4) == 'bb' + color + '0'){//matches key portion of iBeacon UUID for a Tilt
+      changeInterval(tiltInterval, 980); //after finding a Tilt, scan for the next advertisement in 980ms. (assumes a 20ms processing time)
+      digitalPulse(LED2, 1, 3);//flash green LED to indicate a connection
+      NRF.setScan();//stop scanning since tilt has been found
+      //console.log(hexData);
+      var majorValue = parseInt(hexData.substr(36,4),16);//set values to repeat
+      var minorValue = parseInt(hexData.substr(40,4),16);//set values to repeat
+      var uuidValue = parseInt(color + hexData[11], 16);//convert tilt color to hex value for use in UUID
+      console.log(majorValue, minorValue, uuidValue);
+      NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : majorValue, minor : minorValue, rssi : -59 }),{interval:1000});
+      scanCounter = 0;
+    }
+  }, { filters: [{ manufacturerData: { 0x004C: {} } }] });
+  //try to sync
+  if (scanCounter == 1){
+    changeInterval(tiltInterval, 1000);//change to 1 second interval after initial interval of 980ms (assumes 20ms processing time no longer needed)
   }
+  //fast search if not found in sync
+  if (scanCounter == 120){//starting scanning 4x more frequently if tilt not found after 120 tries
+    changeInterval(tiltInterval, 190);
+    scanCounter = 2;//skips changing scan to 1 second interval, keeps scanning 4x
+  }
+  scanCounter++;
+  //console.log(scanCounter);
+  //stop scan after x ms
+  setTimeout(function () { NRF.setScan(); }, 110);//main function to duty cycle scanning, scan is actually only 10ms, assumes 100ms for scanning to start up
 }
-});
-//try to sync
-if (scanCounter == 1){
-  changeInterval(tiltInterval, 1000);
-}
-//fast search if not found in sync
-if (scanCounter == 120){
-  changeInterval(tiltInterval, 190);
-  scanCounter = 2;
-}
-scanCounter++;
-//console.log(scanCounter);
-//stop scan after x ms
-setTimeout(function () { NRF.setScan(); }, 110);
-}
+
+// fix for powersave issue in 2v01 and earlier
+digitalWrite(D7,0); 
+//enable watchdog timer - disabling because when asleep it causes a reboot
+//E.enableWatchdog(5);
 
 // set power
 NRF.setTxPower(4);
 // intialize global variables
-var scanCounter = 0;
-var color = 0;
-var presses = 9;
-var tiltInterval;
-//enable watchdog timer
-E.enableWatchdog(5);
+var scanCounter = 0;//number of scans counted starts from 0
+var color = 0;//no tilt color selected state
+var presses = 9;//not advertising state
+var tiltInterval;//global variable for managing tilt scan frequency
 // remove all watches
 clearWatch();
 //cleart intervals and timeouts
@@ -59,22 +56,22 @@ clearTimeout();
 setWatch(function() {
   presses++;
   //conditional for presses past valid color (8)
-  if (presses == 9){
+  if (presses == 9){//turns off advertising and scanning, flashed red LED to indicate turned off
     NRF.sleep();
     clearInterval(tiltInterval);
     digitalPulse(LED1, 1, 50);
     return;
   }
   if (presses > 9){
-    presses = 1;
+    presses = 1;//start over with presses (color)
     NRF.wake();
-    tiltInterval = setInterval(function (){ startScan(); }, 190);
+    tiltInterval = setInterval(function (){ startScan(); }, 190);//start with frequent scan
     digitalPulse(LED2, 1, 50);
   }else if (presses < 9) {
       digitalPulse(LED2, 1, 50);
   }
   //jump to fast search
-  scanCounter = 120;
+  scanCounter = 120;//skip to 120 to maintain frequent scanning
   var uuidValue = parseInt('0x' + presses + '0');
   //start advertising initial repeated color whith zeros for sg and temp values
   NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : 0, minor : 0, rssi : -59 }),{interval:500});
