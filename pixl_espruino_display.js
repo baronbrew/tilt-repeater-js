195a6147375eaddc25b235d8b937f85c369f0398
Noah Baron <noahbaron@gmail.com>
2019-05-29 10:34:15 -0700

    Added script for a Pixl display only, adjusted fast scan internval
    Fast scan interval of 190ms may be to short to allow time for the scan
    to complete. Updated interval to 240ms.

--- pixl_espruino_display.js
+++ pixl_espruino_display.js
@@ -1,4 +1,4 @@
-//espruino_repeater.js
+//pixl_espruino_display.js
 // Use "save on send" -> "direct to flash" in Web IDE settings
 // Needs 2v00 or above - preferably 2v01 or later
 
@@ -11,15 +11,31 @@ function startScan (){
     var hexData = arrayBufferToHex(d.manufacturerData);
     if (hexData.substr(8,4) == 'bb' + color + '0'){//matches key portion of iBeacon UUID for a Tilt
       changeInterval(tiltInterval, 980); //after finding a Tilt, scan for the next advertisement in 980ms. (assumes a 20ms processing time)
-      digitalPulse(LED2, 1, 3);//flash green LED to indicate a connection
+      //digitalPulse(LED2, 1, 3);//flash green LED to indicate a connection
       NRF.setScan();//stop scanning since tilt has been found
-      //console.log(hexData);
-      var majorValue = parseInt(hexData.substr(36,4),16);//set values to repeat
-      var minorValue = parseInt(hexData.substr(40,4),16);//set values to repeat
-      var uuidValue = parseInt(color + hexData[11], 16);//convert tilt color to hex value for use in UUID
-      console.log(majorValue, minorValue, uuidValue);
-      NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : majorValue, minor : minorValue, rssi : -59 }),{interval:1000});
+      var majorValue = parseInt(hexData.substr(36,4),16);
+      var minorValue = parseInt(hexData.substr(40,4),16);
       scanCounter = 0;
+//display stuff for pixl
+var sg = (minorValue/1000).toFixed(3);
+var t = majorValue;
+      // Clear display
+  g.clear();
+  // Use the small font for a title
+  g.setFontBitmap();
+  g.drawString("SG (pre-cal):",0,0);
+  // Use a large font for the value itself
+  g.setFontVector(22);
+  g.drawString(sg, (g.getWidth()-g.stringWidth(sg))/1,7);
+  //temperature
+  // Use the small font for a title
+  g.setFontBitmap();
+  g.drawString("Temp. (pre-cal):",0,33);
+  // Use a large font for the value itself
+  g.setFontVector(22);
+  g.drawString(t + "°F", (g.getWidth()-g.stringWidth(t + " F"))/1,40);
+  // Update the screen
+  g.flip();   
     }
   }, { filters: [{ manufacturerData: { 0x004C: {} } }] });
   //try to sync
@@ -37,11 +53,6 @@ function startScan (){
   setTimeout(function () { NRF.setScan(); }, 110);//main function to duty cycle scanning, scan is actually only 10ms, assumes 100ms for scanning to start up
 }
 
-// fix for powersave issue in 2v01 and earlier
-digitalWrite(D7,0); 
-//enable watchdog timer - disabling because when asleep it causes a reboot
-//E.enableWatchdog(5);
-
 // set power
 NRF.setTxPower(4);
 // intialize global variables
@@ -55,6 +66,7 @@ clearWatch();
 clearInterval();
 clearTimeout();
 NRF.setAdvertising({},{});
+
 setWatch(function() {
   presses++;
   //conditional for presses past valid color (8)
@@ -67,15 +79,14 @@ setWatch(function() {
   if (presses > 9){
     presses = 1;//start over with presses (color)
     NRF.wake();
-    tiltInterval = setInterval(function (){ startScan(); }, 240);//start with frequent scan
-    digitalPulse(LED2, 1, 50);
+    tiltInterval = setInterval(function (){ startScan(); }, 240);//start with frequent scan (min. interval without memory error is 240ms)
+    //digitalPulse(LED2, 1, 50);
+    console.log(presses);
   }else if (presses < 9) {
-      digitalPulse(LED2, 1, 50);
+      //digitalPulse(LED2, 1, 50);
+    console.log(presses);
   }
   //jump to fast search
   scanCounter = 120;//skip to 120 to maintain frequent scanning
-  var uuidValue = parseInt('0x' + presses + '0');
-  //start advertising initial repeated color whith zeros for sg and temp values
-  NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : 0, minor : 0, rssi : -59 }),{interval:500});
   color = presses;
 }, BTN, { repeat: true, debounce : 50, edge: "rising" });
