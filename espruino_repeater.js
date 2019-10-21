dc7343fe6a509a4314f34e5b6efa087554cf4bc6
Noah Baron <noahbaron@gmail.com>
2019-10-21 06:52:29 -0700

    Timeout and Power Save Feature
    After 7 scanning cycles without connecting, the repeater will now set
    the sg and temp to 0 and go into a lower power (scanning every 10
    seconds instead of every 240ms) mode for 2 cycles.

--- espruino_repeater.js
+++ espruino_repeater.js
@@ -16,10 +16,11 @@ function startScan (){
       //console.log(hexData);
       var majorValue = parseInt(hexData.substr(36,4),16);//set values to repeat
       var minorValue = parseInt(hexData.substr(40,4),16);//set values to repeat
-      var uuidValue = parseInt(color + hexData[11], 16);//convert tilt color to hex value for use in UUID
+      uuidValue = parseInt(color + hexData[11], 16);//convert tilt color to hex value for use in UUID
       console.log(majorValue, minorValue, uuidValue);
       NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : majorValue, minor : minorValue, rssi : -59 }),{interval:1000});
       scanCounter = 0;
+      scanCycle = 0;
     }
   }, { filters: [{ manufacturerData: { 0x004C: {} } }] });
   //try to sync
@@ -28,13 +29,20 @@ function startScan (){
   }
   //fast search if not found in sync
   if (scanCounter == 120){//starting scanning 4x more frequently if tilt not found after 120 tries
+    scanCycle++;
     changeInterval(tiltInterval, 240);
     scanCounter = 2;//skips changing scan to 1 second interval, keeps scanning 4x
   }
   scanCounter++;
   //console.log(scanCounter);
-  //stop scan after x ms
-  setTimeout(function () { NRF.setScan(); }, 110);//main function to duty cycle scanning, scan is actually only 10ms, assumes 100ms for scanning to start up
+  //go to power saving mode and reset sg and temp to 0 after 5 scan cycles
+  if (scanCycle % 10 === 7 && scanCounter == 3){
+    changeInterval(tiltInterval, 10000);
+    NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : 0, minor : 0, rssi : -59 }),{interval:1000});
+  }else if (scanCycle % 10 === 9 && scanCounter == 3){
+  changeInterval(tiltInterval, 240);
+  }
+  setTimeout(function () { NRF.setScan(); }, 110);
 }
 
 // fix for powersave issue in 2v01 and earlier
@@ -45,6 +53,8 @@ digitalWrite(D7,0);
 // set power
 NRF.setTxPower(4);
 // intialize global variables
+var scanCycle = 0;
+var uuidValue = 0x00;
 var scanCounter = 0;//number of scans counted starts from 0
 var color = 0;//no tilt color selected state
 var presses = 9;//not advertising state
@@ -74,7 +84,7 @@ setWatch(function() {
   }
   //jump to fast search
   scanCounter = 120;//skip to 120 to maintain frequent scanning
-  var uuidValue = parseInt('0x' + presses + '0');
+  uuidValue = parseInt('0x' + presses + '0');
   //start advertising initial repeated color whith zeros for sg and temp values
   NRF.setAdvertising(require("ble_ibeacon").get({ uuid : [0xa4, 0x95, 0xbb, uuidValue, 0xc5, 0xb1, 0x4b, 0x44, 0xb5, 0x12, 0x13, 0x70, 0xf0, 0x2d, 0x74, 0xde], major : 0, minor : 0, rssi : -59 }),{interval:500});
   color = presses;
